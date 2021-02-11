e32a0159ed1c84533d2b2234556fa3cede5bdc6a
kartikaysharma01 <sharma.kartik2107@gmail.com>
2021-02-11 21:32:42 +0530

    bubble sort implemented

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,35 +1,35 @@
+
 public class BubbleSort {
-	
-	 void bubbleSort(int arr[]) 
-	    { 
-	        int n = arr.length; 
-	        for (int i = 0; i < n-1; i++) 
-	            for (int j = 0; j < n-i-1; j++) 
-	                if (arr[j] > arr[j+1]) 
-	                { 
-	                    // swap arr[j+1] and arr[j] 
-	                    int temp = arr[j]; 
-	                    arr[j] = arr[j+1]; 
-	                    arr[j+1] = temp; 
-	                } 
-	    } 
-	  
-	    /* Prints the array */
-	    void printArray(int arr[]) { 
-	        int n = arr.length; 
-	        for (int i=0; i<n; ++i) 
-	            System.out.print(arr[i] + " "); 
-	        System.out.println(); 
+
+	 void bsort(int  num[])
+	    {
+	        int n = num.length;
+	        for (int i = 0; i < n-1; i++)
+	            for (int j = 0; j < n-i-1; j++)
+	                if (num[j] > num[j+1])
+	                {
+	                    int temp = num[j];
+	                    num[j] = num[j+1];
+	                    num[j+1] = temp;
+	                }
 	    }
 	    
-	   
+	    void display(int num[])
+	    {
+	        int n = num.length;
+	        for (int i=0; i<n; ++i)
+	            System.out.print(num[i] + " ");
+	        System.out.println();
+	    }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		   BubbleSort ob = new BubbleSort(); 
-	        int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
-	        ob.bubbleSort(arr); 
-	        System.out.println("Sorted array"); 
-	        ob.printArray(arr); 
+			
+		BubbleSort bs = new BubbleSort();
+	    int num[] = {64, 34, 25, 12, 22, 11, 90};
+	    bs.bsort(num);
+	    System.out.println("Sorted array");
+	    bs.display(num);
+
 	}
 
 }
